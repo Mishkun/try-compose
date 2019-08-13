1e3256cf9e8f3a9bce20a5d068699831bacb5dcd
Mikhail Levchenko <mishkun.lev@gmail.com>
2019-08-13 23:57:10 +0300

    Add latest prepackaged libraries to the repo

--- app/src/main/java/dev/mishkun/trycompose/MainActivity.kt
+++ app/src/main/java/dev/mishkun/trycompose/MainActivity.kt
@@ -3,12 +3,12 @@ package dev.mishkun.trycompose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.compose.*
-import androidx.ui.core.CraneWrapper
+import androidx.ui.core.setContent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { CraneWrapper { SimpleComposable() } }
+        setContent { SimpleComposable() }
     }
 }
